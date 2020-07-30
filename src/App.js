5c0c33ce6688f384818f29273f648bbdb5c1362d
Aneta Szynal <anetaszynal28@gmail.com>
2020-07-30 23:04:42 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import Container from "./Container";
+import Container from "./Container"
 import Header from "./Header";
+import Section from "./Section";
+import Form from "./Section/Form";
+import Result from "./Section/Result";
+import Footer from "./Footer";
 
 function App() {
   return (
     <Container>
-      <Header title="Internetowy kantor">
-      </Header>
-
+      <Header title="Internetowy kantor" />
+      <Section
+        title="Wprowadź dane:"
+        body={<Form />}
+      >
+      </Section>
+      <Section
+        title="Wynik:"
+        body={<Result />}
+      >
+      </Section>
+      <Footer
+        title="Kontakt:">
+      </Footer>
     </Container>
   );
 };
