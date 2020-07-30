c0801bf4f7c777232b6660a45e31f0d125822472
Aneta Szynal <anetaszynal28@gmail.com>
2020-07-30 15:09:21 +0200

    add header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import Container from "./Container";
+import Header from "./Header";
 
 function App() {
   return (
     <Container>
+      <Header title="Internetowy kantor">
+      </Header>
 
     </Container>
   );
