1155db23e0392e85913ee993516cab776a66fbc0
Aneta Szynal <anetaszynal28@gmail.com>
2020-07-30 23:00:48 +0200

    add table of currencies

---
+++ src/currencies.js
@@ -0,0 +1,22 @@
+export const currencies = [
+  {
+    shortname: "PLN",
+    name: "Złoty",
+    rate: 1,
+  },
+  {
+    shortname: "EUR",
+    name: "Euro",
+    rate: 4.4090,
+  },
+  {
+    shortname: "GBP",
+    name: "Funt",
+    rate: 4.8761,
+  },
+  {
+    shortname: "USD",
+    name: "Dolar",
+    rate: 3.7549,
+  },
+]
