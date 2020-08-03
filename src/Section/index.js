950acdd0bc42a29b983c8d45dc56ba1582d0d008
Aneta Szynal <anetaszynal28@gmail.com>
2020-08-03 22:30:59 +0200

    add border to container, remove border from footer

--- src/Section/index.js
+++ src/Section/index.js
@@ -3,7 +3,7 @@ import "./style.css"
 
 const Section = ({ title, body }) => (
   <section className="section">
-    <h2 class="section__title">{title}</h2>
+    <h2 className="section__title">{title}</h2>
     <div>{body}</div>
   </section>
 );
