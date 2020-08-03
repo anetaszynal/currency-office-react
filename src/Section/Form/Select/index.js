46645d671f07592f081bbef94d84690e347e652d
Aneta Szynal <anetaszynal28@gmail.com>
2020-08-03 22:36:24 +0200

    add possibility to change value

--- src/Section/Form/Select/index.js
+++ src/Section/Form/Select/index.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { currencies } from "../../../currencies"
 
-const Select = ({ title }) => (
-  <label className="form__label">
-    <p className="form__paragraph">{title}</p>
-    <select className="form__field" name="currencies">
-      {currencies.map(({ name, shortname }) => (
-        <option
-          key={shortname}
-          value={name}>
-          {name}
-        </option>)
-      )}
-    </select>
-  </label>
-);
+const Select = ({ title, onSelectChange, currency }) => {
+  return (
+    <label className="form__label">
+      <p className="form__paragraph">{title}</p>
+      <select
+        className="form__field"
+        value={currency}
+        onChange={onSelectChange}>
+        {currencies.map((currency => (
+          <option
+            key={currency.shortname}
+            value={currency.shortname}
+          >
+            {currency.name}
+          </option>
+        )))}
+      </select>
+    </label>
+  );
+};
 export default Select;
+
