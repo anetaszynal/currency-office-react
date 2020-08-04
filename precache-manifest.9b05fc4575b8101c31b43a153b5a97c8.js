24a95bca2330162d7440b09af30f17adeaa4aca5
Aneta Szynal <anetaszynal28@gmail.com>
2020-08-04 19:42:47 +0200

    Updates

---
+++ precache-manifest.9b05fc4575b8101c31b43a153b5a97c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05408a545333d15f81e55929ad446574",
+    "url": "/currency-office-react/index.html"
+  },
+  {
+    "revision": "66fd29b18a2a90409121",
+    "url": "/currency-office-react/static/css/main.236a5ae9.chunk.css"
+  },
+  {
+    "revision": "9acd0fdac40622843899",
+    "url": "/currency-office-react/static/js/2.2bc4ca84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/currency-office-react/static/js/2.2bc4ca84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66fd29b18a2a90409121",
+    "url": "/currency-office-react/static/js/main.63d9b95f.chunk.js"
+  },
+  {
+    "revision": "b7b90f348dfd023fd92e",
+    "url": "/currency-office-react/static/js/runtime-main.2b2a611d.js"
+  },
+  {
+    "revision": "63038f351ba44523a236c0f550da6ee2",
+    "url": "/currency-office-react/static/media/background.63038f35.jpg"
+  }
+]);
