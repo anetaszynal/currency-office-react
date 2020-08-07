856a2f0d8c36ea32be1c4a5d43efa1adb4fbef75
Aneta Szynal <anetaszynal28@gmail.com>
2020-08-07 23:24:17 +0200

    feat(): add icon and og image

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./style.css";
 
-export const Container = ({children}) => (
-  <main className="container"> 
+export const Container = ({ children }) => (
+  <main className="container">
     {children}
   </main>
 );
