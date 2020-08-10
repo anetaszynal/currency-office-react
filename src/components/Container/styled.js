748d325f961d9cad80fef7cb0cff63f5fd032869
Aneta Szynal <anetaszynal28@gmail.com>
2020-08-10 22:58:54 +0200

    refactor(Container): add styled-components

--- src/components/Container/styled.js
+++ src/components/Container/styled.js
@@ -1,7 +1,10 @@
-.container {
+import styles from 'styled-components';
+import styled from 'styled-components';
+
+export const Content = styled.main`
   max-width: 80%;
   margin: 20px auto;
   background-color: white;
   border: 2px inset rgb(236 236 236);
   box-shadow: 0px 0px 5px 2px #767171;
-}
+`;
